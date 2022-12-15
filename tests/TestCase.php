c38e34d92412a5516d1b37121095f05aab821faa
Nurudeen <ajayi.nurudeen.wale@gmail.com>
2022-12-15 23:06:30 +0000

    Addes base setup and mockers for admin and non-users - customers or clients which is used in asserting and simulating user activity.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,78 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Adds a simple setup that runs on every test caes.
+     * 
+     * @return void
+     */
+    public function setUp() : void
+    {
+        parent::setUp();        
+    }
+
+    /**
+     * Mocks and acts like a non-admin user factory.
+     * 
+     * @param $user
+     * 
+     */
+    protected function asNonAdmin($user = null)
+    {
+        $this->actingAs($this->nonAdmin(), 'web');
+
+        return $this;
+    }
+
+    /**
+     * Creates an non-admin user factory.
+     * 
+     * @param $user
+     * 
+     */
+    protected function nonAdmin($user = null)
+    {
+        return $this->nonAdmin ?? match (true) {
+            $user instanceof User => $user,
+            $user instanceof UserFactory => $user->create(),
+            default => User::factory()->user()->create()
+        };
+    }
+
+    /**
+     * Mocks and acts like an admin.
+     * 
+     * @param $user
+     * 
+     */
+    protected function asAdmin($user=null)
+    {
+        $this->actingAs($this->admin(), 'web');
+
+        return $this;
+    }
+
+    /**
+     * Creates an admin user factory.
+     * 
+     * @param $user
+     * 
+     */
+    protected function admin($user = null)
+    {
+        return $this->admin ?? match (true) {
+            $user instanceof User => $user,
+            $user instanceof UserFactory => $user->create(),
+            default => User::factory()->admin()->create()
+        };
+    }
+
 }
