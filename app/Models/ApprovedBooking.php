541e0b43184e3c0999349d7946af3d63e919d57c
Nurudeen <ajayi.nurudeen.wale@gmail.com>
2022-12-15 23:49:51 +0000

    Updated approved booking model to have necessary relationship defined with a booking

--- app/Models/ApprovedBooking.php
+++ app/Models/ApprovedBooking.php
@@ -9,4 +9,15 @@ use Illuminate\Database\Eloquent\Model;
 class ApprovedBooking extends Model
 {
     use HasFactory, HasUuid;
+
+    protected $fillable = ['booking_id', 'approved_by'];
+
+    /**
+     * Returns the associated shipper
+     * 
+     */
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
 }
