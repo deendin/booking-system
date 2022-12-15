f4b9a62c20b419b1cf9a49c1a8bf1ebe1fee6b2e
Nurudeen <ajayi.nurudeen.wale@gmail.com>
2022-12-15 23:50:13 +0000

    Added specific routes for booking controller and admin related controller logic

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\BookingController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\BookingController as AdminBookingController;
+use App\Http\Controllers\Admin\ApprovedBookingController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [BookingController::class, 'create'])->name('booking.index');
 Route::post('/booking/store', [BookingController::class, 'store'])->name('booking.store');
 
+// ADMIN ROUTES
+Route::post('/admin/auth/login', [AdminController::class, 'login'])->name('admin.auth.login');
+Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
+
+Route::prefix('admin')->name('admin.')->group(
+    function () {
+        // admin routes -> /admin/{route}
+        Route::get('login', [AdminController::class, 'index'])->name('login');
+
+        // admin/bookings -> /admin/bookings/{route}
+        Route::middleware('auth')->prefix('bookings')->group(
+            function () {
+                Route::get('/', [AdminBookingController::class, 'index'])->name('booking.index');
+                Route::get('/edit/{uuid}', [AdminBookingController::class, 'edit'])->name('booking.edit');
+                Route::post('/update/{booking}', [AdminBookingController::class, 'update'])->name('booking.update');
+                Route::get('/destroy/{booking}', [AdminBookingController::class, 'destroy'])->name('booking.destroy');
+                Route::get('/approve/{uuid}', [ApprovedBookingController::class, 'approve'])->name('booking.approve_booking');
+                Route::get('/approved', [ApprovedBookingController::class, 'index'])->name('booking.approved');
+            }
+        );
+        
+    }
+);
